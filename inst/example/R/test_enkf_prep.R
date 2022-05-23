73ea647d2cf89b03bea78f5bd0708b35d77860ee
Tadhg Moore <tadhgm@vt.edu>
2022-05-23 10:06:25 -0400

    Updated library FLAREr -> FLARErLER

--- inst/example/R/test_enkf_prep.R
+++ inst/example/R/test_enkf_prep.R
@@ -1,5 +1,5 @@
 
-template_folder <- system.file("example", package = "FLAREr")
+template_folder <- system.file("example", package = "FLARErLER")
 temp_dir <- tempdir()
 # dir.create("example")
 file.copy(from = template_folder, to = temp_dir, recursive = TRUE)
@@ -11,7 +11,7 @@ lake_directory <- test_directory
 configure_run_file <- "configure_run.yml"
 config_set_name <- "default"
 
-config <- FLAREr::set_configuration(configure_run_file,
+config <- FLARErLER::set_configuration(configure_run_file,
                                     lake_directory,
                                     config_set_name = config_set_name)
 config$run_config$forecast_start_datetime <- "2018-10-05 00:00:00"
@@ -21,21 +21,21 @@ config$da_setup$ensemble_size <- 21
 config$model_settings$ncore <- 2
 config$model_settings$use_ler <- FALSE
 
-noaa_forecast_path <- FLAREr::get_driver_forecast_path(config,
+noaa_forecast_path <- FLARErLER::get_driver_forecast_path(config,
                                                        forecast_model = config$met$forecast_met_model)
 
-inflow_forecast_path <- FLAREr::get_driver_forecast_path(config,
+inflow_forecast_path <- FLARErLER::get_driver_forecast_path(config,
                                                          forecast_model = config$inflow$forecast_inflow_model)
 
 if(!is.null(noaa_forecast_path)){
-  # FLAREr::get_driver_forecast(lake_directory, forecast_path = noaa_forecast_path)
+  # FLARErLER::get_driver_forecast(lake_directory, forecast_path = noaa_forecast_path)
   forecast_dir <- file.path(config$file_path$noaa_directory, noaa_forecast_path)
 }else{
   forecast_dir <- NULL
 }
 
 if(!is.null(inflow_forecast_path)){
-  # FLAREr::get_driver_forecast(lake_directory, forecast_path = inflow_forecast_path)
+  # FLARErLER::get_driver_forecast(lake_directory, forecast_path = inflow_forecast_path)
   inflow_file_dir <- file.path(config$file_path$noaa_directory,inflow_forecast_path)
 }else{
   inflow_file_dir <- NULL
@@ -53,7 +53,7 @@ observed_met_file <- file.path(config$file_path$qaqc_data_directory,
                                paste0("observed-met_",config$location$site_id,".nc"))
 
 #Step up Drivers
-met_out <- FLAREr::generate_met_files(obs_met_file = observed_met_file,
+met_out <- FLARErLER::generate_met_files(obs_met_file = observed_met_file,
                                       out_dir = config$file_path$execute_directory,
                                       forecast_dir = forecast_dir,
                                       config = config)
@@ -63,7 +63,7 @@ historical_met_error <- met_out$historical_met_error
 
 inflow_forecast_path <- config$file_path$inflow_directory
 
-inflow_outflow_files <- FLAREr::create_inflow_outflow_files(inflow_file_dir = inflow_file_dir,
+inflow_outflow_files <- FLARErLER::create_inflow_outflow_files(inflow_file_dir = inflow_file_dir,
                                                             inflow_obs = cleaned_inflow_file,
                                                             working_directory = config$file_path$execute_directory,
                                                             config,
@@ -72,7 +72,7 @@ inflow_outflow_files <- FLAREr::create_inflow_outflow_files(inflow_file_dir = in
 inflow_file_names <- inflow_outflow_files$inflow_file_name
 outflow_file_names <- inflow_outflow_files$outflow_file_name
 
-states_config <- FLAREr::generate_states_to_obs_mapping(states_config, obs_config)
-model_sd <- FLAREr::initiate_model_error(config, states_config)
+states_config <- FLARErLER::generate_states_to_obs_mapping(states_config, obs_config)
+model_sd <- FLARErLER::initiate_model_error(config, states_config)
 
 # end
